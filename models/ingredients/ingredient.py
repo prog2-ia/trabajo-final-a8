2e3ca183e255db0e67bb71e3b9d51b778293c57a
Tu Nombre <aic35@alu.ua.es>
2026-02-23 14:06:57 +0100

    prog_modular

--- models/ingredients/ingredient.py
+++ models/ingredients/ingredient.py
@@ -4,13 +4,7 @@ class Ingredient(ABC):
     """
     Clase abstracta que define que propiedades van a tener los ingredientes
     """
-
-    def __init__(self, nombre: str, cantidad: int, alergenos=None, categoria= None) -> None:
-        self.
-
-    @property
-    def origen(self):
-        return self.__origen
+    pass
     
 
 
