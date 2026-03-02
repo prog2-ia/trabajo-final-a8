c5443f5963fed9f8ae8d51809202e09736fff77f
Alfonso <aic35@alu.ua.es>
2026-03-02 13:50:08 +0100

    ingredient_update

--- models/ingredients/animal_ingredient.py
+++ models/ingredients/animal_ingredient.py
@@ -1,5 +1,14 @@
 from ingredient import Ingredient
 
 class AnimalIngredient(Ingredient):
-    pass
+    def __init__(self, id: int, origen: str, fecha_caducidad: str, proteinas: float):
+        super().__init__(id, origen, fecha_caducidad)
+        self.__proteinas = proteinas
+
+    @property
+    def proteinas(self):
+        return self.__proteinas
+    
+    def __str__(self):
+        pass
 
--- models/ingredients/ingredient.py
+++ models/ingredients/ingredient.py
@@ -4,7 +4,52 @@ class Ingredient(ABC):
     """
     Clase abstracta que define que propiedades van a tener los ingredientes
     """
-    pass
+    def __init__(self, id: int, origen: str, fecha_caducidad: str, macro_nutrientes: list, alergenos: list):
+        self.__id = id
+        self.__origen = origen
+        self.__fecha_caducidad = fecha_caducidad
+        self.__macro_nutrientes = macro_nutrientes
+        self.__alergenos = alergenos
+
+    @property
+    def id(self):
+        return self.__id
+        
+    @property
+    def origen(self):
+        return self.__origen
+        
+    @property
+    def fecha_caducidad(self):
+        return self.__fecha_caducidad
+
+    @property
+    def marco_nutrientes(self):
+        return self.__macro_nutrientes
+    
+    @marco_nutrientes.setter
+    def macro_nutrientes(self, nutrientes: list):
+        if not nutrientes.strip():
+            raise ValueError("La lista de nutrientes no puede estar vacía")
+        else:
+            self.macro_nutrientes.append(nutrientes)
+
+    @property
+    def alergenos(self):
+        return self.__alergenos
+    
+    @alergenos.setter
+    def alergenos(self, lista_alergenos: list):
+        if not lista_alergenos.strip():
+            raise ValueError("La lista de alérgenos no puede estar vacía")
+
+    def __str__(self):
+        return f"Información de ingrediente: id={self.id}, origen={self.origen},
+                 fecha_caducidad={self.fecha_caducidad}, Macro-Nutrientes={self.macro_nutrientes}, 
+                 Alérgenos={self.alergenos}"
+
+
+
     
 
 
