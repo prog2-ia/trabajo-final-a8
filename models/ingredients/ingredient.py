69560b7ea42ad9dc7760b1a1fdb1016a4459e106
AlfonsoÍñiguez <aic35@alu.ua.es>
2026-03-07 20:00:10 +0100

    Update ingredient.py

--- models/ingredients/ingredient.py
+++ models/ingredients/ingredient.py
@@ -6,18 +6,97 @@ class Ingredient(ABC):
 
     Atributos
     ---------
+        name: 
+        quantity: 
+        calories_per_100g:
+        type:
+        allergens: 
 
-    id -> Número de identificación del alimento
-    origen -> "ANIMAL", "VEGETAL" o "MINERAL"
-    fecha_caducidad -> Fecha de caducidad ("dd/mm/yyy")
-    macro_nutrientes -> Diccionario de marconutrientes que aporta el alimento
-    Alergenos -> Lista de los alérgenos del alimento
-
-    Funciones
+    Métodos
     ---------
-    str -> Muestra la información detalla del alimento
-
+        total_calories: devuelve el número de calorías según la cantidad
+        is_allergen: devuelve True si el alimento contiene el alérgeno indicado
+        str -> Muestra la información detalla del alimento
     """
+
+    def __init__(self, name: str, quantity: float, calories_per_100g: float, type: str, allergens: list[str]):
+        self.__name = name
+        self.quantity = quantity
+        self.calories_per_100g = calories_per_100g
+        self.type = type
+        self.allergens = allergens
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def quantity(self):
+        return self.__quantity
+    
+    @quantity.setter
+    def quantity(self, value):
+        if value <= 0:
+            raise ValueError("Cantidad ha de ser mayor a 1 gramo")
+        else:
+            self.__quantity = value
+        
+    @property
+    def calories_per_100g(self):
+        return self.__calories_per_100g
+    
+    @calories_per_100g.setter
+    def calores_per_100g(self, value):
+        if value <= 0:
+            raise ValueError("EL número de calorías ha de ser mayor que 0")
+        else:
+            self.__calories_per_100g = value
+
+    @property
+    def type(self):
+        return self.__type
+    
+    @type.setter
+    def type(self, value):
+        if value not in ("ANIMAL", "PLANTA", "MINERAL"):
+            raise ValueError("Tipo de alimento no disponible")
+        else:
+            self.__type = value
+
+    @property
+    def allergens(self):
+        return self.__allergens
+    
+    @allergens.setter
+    def allergens(self, value):
+        if not isinstance(value, list):
+            raise ValueError("Hay que pasar los alérgenos empleando una lista")
+
+        for allergen in value:
+            if not isinstance(allergen, str):
+                raise ValueError("No introduzca números porfavor")
+            
+        else:
+            self.__allergens += value
+
+    
+    def total_calories(self):
+        return self.calories_per_100g * self.quantity
+    
+    def is_allergen(self, allergen: str):
+        if not isinstance(allergen, str):
+            raise ValueError("Introduzca el alérgeno con letras porfavor")
+        else:
+            for a in self.allergens:
+                if allergen == a:
+                    return True
+        return False 
+    
+    def __str__(self):
+        return f"Información alimento: Ingredient[name='{self.name}', \
+                quantity={self.quantity}, calories_per_100g={self.calories_per_100g}, \
+                allergens='{self.allergens}']"
+
 
 
 
