1839547c11e80dc5175aa1537b9e4f92a9261bae
AlfonsoÍñiguez <aic35@alu.ua.es>
2026-03-07 23:08:53 +0100

    Update mixed_dish.py

--- models/dishes/mixed_dish.py
+++ models/dishes/mixed_dish.py
@@ -1,4 +1,29 @@
-from dish import Dish
+from .dish import Dish
+from ingredients import Ingredient
 
 class MixedDish(Dish):
-    pass
+    """
+    Clase que define los platos combinados
+    """
+
+    def __init__(self, name, ingredients, servings):
+        super().__init__(name, ingredients, servings, "MIXTO")
+
+    def is_vegan(self):
+        for ingredient in self.ingredients:
+            if ingredient.type not in ("PLANTA", "MINERAL"):
+                return False
+        return True
+    
+    def is_meat(self):
+        for ingredient in self.ingredients:
+            if ingredient.type != "ANIMAL":
+                return False
+        return True
+    
+    def add_ingredient(self, ingredient: Ingredient):
+        try:
+            super().add_ingredient(ingredient)
+        except ValueError as e:
+            return f"No se ha podido añadir al ingrediente al plato mixto \
+                    Error: {e}"
