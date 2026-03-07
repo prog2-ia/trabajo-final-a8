a6ef5b78357f9a0a836c0470b4827564e8ce88f4
AlfonsoÍñiguez <aic35@alu.ua.es>
2026-03-07 23:16:17 +0100

    Update vegan_dish.py

--- models/dishes/vegan_dish.py
+++ models/dishes/vegan_dish.py
@@ -1,4 +1,26 @@
-from dish import Dish
+from .dish import Dish
+from ingredients import Ingredient
 
 class VeganDish(Dish):
-    pass
+    """
+    Clase que define los platos veganos
+    """
+
+    def __init__(self, name, ingredients, servings):
+        super().__init__(name, ingredients, servings, "VEGANO")
+
+    def is_vegan(self):
+        for ingredient in self.ingredients:
+            if ingredient.type != ("PLANTA", "MINERAL"):
+                return False
+        return True
+
+    def is_meat(self):
+        return False
+
+    def add_ingredient(self, ingredient: Ingredient):
+        if ingredient.type not in ("PLANTA", "MINERAL"):
+            raise ValueError("Solo se permiten alimentos de tipo planta o mineral")
+        else:
+            super().add_ingredient(ingredient) 
+
