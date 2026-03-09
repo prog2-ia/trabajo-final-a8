fc69df84f224546e81600ec82bc0013849403995
Alfonso <aic35@alu.ua.es>
2026-03-09 13:42:15 +0100

    pdf_update

--- persistence/menu_pdf.py
+++ persistence/menu_pdf.py
@@ -1,7 +1,33 @@
+# importamos la clase FPDF
 from fpdf import FPDF
 
+# Instanciamos FPDF
 pdf = FPDF()
-pdf.add_page()
-pdf.set_font("helvetica", style="B", size=16)
-pdf.cell(40, 10, "Hello World!")
-pdf.output("prueba.pdf")
+
+# Definimos la función
+"""
+La función recibe cuatro argumentos:
+text: texto o contenido del documento.
+font_size: tamaño de letras.
+title_pdf: titulo del PDF.
+doc_name: nombre del archivo.
+"""
+def generador_pdf(text, font_size, title_pdf, doc_name):
+    # Agregamos una pagina en blanco.
+    pdf.add_page()
+
+    # Definimos el titulo del documento
+    pdf.set_title(title_pdf)
+
+    # Definimos el tipo de fuente y el tamaño
+    pdf.set_font("Arial", size=font_size)
+
+    # Agregamos una celda con sus coordenadas (100, 20)
+    # Y dfinimos el texto
+    pdf.cell(100, 20, txt=text)
+
+    # Generamos la salida del documento.
+    pdf.output(doc_name)
+
+# Llamamos a la función con sus respectivos argumentos
+generador_pdf("Menú", 50, "Documento de Geek´s Acamdey", "prueba.pdf")
